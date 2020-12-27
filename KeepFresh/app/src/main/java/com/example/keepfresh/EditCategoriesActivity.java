581a14c6a028132342fcccec749b22a4d0d45ef2
LAPTOP-N52NH411\madal <madalinazbr@gmail.com>
2020-12-27 12:48:04 +0200

    Hardcoded values for CATEGORIES

--- KeepFresh/app/src/main/java/com/example/keepfresh/EditCategoriesActivity.java
+++ KeepFresh/app/src/main/java/com/example/keepfresh/EditCategoriesActivity.java
@@ -2,40 +2,47 @@ package com.example.keepfresh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
-//import android.view.View;
-//import android.widget.ArrayAdapter;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.ListView;
-//import android.widget.Toast;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EditCategoriesActivity extends AppCompatActivity {
-//
-//    private EditText categoryName;
-//    private Button addNewCategoryButton;
-//    private ListView listViewCategories;
+
+    private EditText categoryName;
+    private Button addNewCategoryButton;
+    private ListView listViewCategories;
+
+    private Context context;
+
+    private static final String[] list = new String[]{"food", "pills"};
 //    private KeepFreshDatabaseHelper keepFreshDatabaseHelper;
-//
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_categories);
-//
-//        categoryName = findViewById(R.id.add_new_categories_field);
-//        addNewCategoryButton = findViewById(R.id.add_new_category_button);
-//        listViewCategories = findViewById(R.id.list_view_categories);
-//
-////        String[] list = new String[]{"food", "pills"};
-////        EditCategoriesCustomAdapter editCategoriesCustomAdapter = new EditCategoriesCustomAdapter(getApplicationContext(), list);
-////        listViewCategories.setAdapter(editCategoriesCustomAdapter);
-////        listViewCategories.setAdapter(new EditCategoriesCustomAdapter(this, new String[]{"food", "pills"}));
-//
+
+        context = this;
+
+        categoryName = findViewById(R.id.add_new_categories_field);
+        addNewCategoryButton = findViewById(R.id.add_new_category_button);
+        listViewCategories = findViewById(R.id.list_view_categories);
+
+
+//        EditCategoriesCustomAdapter editCategoriesCustomAdapter = new EditCategoriesCustomAdapter(getApplicationContext(), list);
+//        listViewCategories.setAdapter(editCategoriesCustomAdapter);
+        listViewCategories.setAdapter(new EditCategoriesCustomAdapter(this, list));
+
 //        keepFreshDatabaseHelper = KeepFreshDatabaseHelper.getInstance(this);
-//
+
 //        addNewCategoryButton.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
